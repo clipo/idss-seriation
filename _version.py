62cc242e0b54daa80de6f839ee99a1e06ebb480a
Mark Madsen <mark@madsenlab.org>
2015-08-31 17:12:26 -0700

    Cleanup, this moved into the module tree so that the modules could import from it at runtime.

--- _version.py
+++
@@ -1,4 +0,0 @@
-
-# This file is originally generated from Git information by running 'setup.py
-# version'. Distribution tarballs contain a pre-generated copy of this file.
-__version__ = '2.2-10-gd20b107-dirty'
